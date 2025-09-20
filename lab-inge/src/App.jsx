0a8e9ce0a973e0a7055a3aaaeffde15e6e8d5bb0
toni <antonio.garcia@mi.unc.edu.ar>
2025-09-20 04:10:11 -0300

    add tailwind

---
+++ lab-inge/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div class="min-h-screen bg-slate-800 flex">
+      <h1 class="text-4xl m-auto text-slate-500 hover:text-slate-300 transition">
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
