21492ddef053781d003f5af96fcdb644f4ae94e8
toni <antonio.garcia@mi.unc.edu.ar>
2025-09-23 02:12:11 -0300

    commit inicial

--- lab-inge/src/containers/App/App.jsx
+++ lab-inge/src/containers/App/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
-import Home from '../../components/HomePage.jsx';
+import './App.css'
 
 function App() {
+
   return (
-    <div >
-       <Home />
+    <div class="min-h-screen bg-slate-800 flex">
+      <h1 class="text-4xl m-auto text-slate-500 hover:text-slate-300 transition">
+        Hello World!
+      </h1>
     </div>
-  );
+  )
 }
 
 export default App;
