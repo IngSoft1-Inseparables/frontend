e28a20dffbe97ffd51e202bd49a4ece6e1b48851
candelalu <candela.saracho@mi.unc.edu.ar>
2025-09-28 20:16:52 +0000

    ING-5 add componente DiscardDeck y modifico el Decks.jsx para probar el componente nuevo

--- lab-inge/src/pages/Decks.jsx
+++ lab-inge/src/pages/Decks.jsx
@@ -1,8 +1,10 @@
 import RegularDeck from "../components/RegularDeck/RegularDeck"
+import DiscardDeck from "../components/DiscardDeck/DiscardDeck"
 
 function Decks() {
   return (
     <div>
+        <DiscardDeck/>
         <RegularDeck />
     </div>
     
