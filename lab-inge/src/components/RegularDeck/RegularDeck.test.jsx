a87709661d891db8053c3ae3652e899b788d80a1
candelalu <candela.saracho@mi.unc.edu.ar>
2025-09-29 14:39:57 +0000

    ING-3 add test para componente RegularDeck

--- lab-inge/src/components/RegularDeck/RegularDeck.test.jsx
+++ lab-inge/src/components/RegularDeck/RegularDeck.test.jsx
@@ -1,26 +1,30 @@
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import RegularDeck from './RegularDeck.jsx'
 import '@testing-library/jest-dom'
-import { expect } from 'vitest/dist/index.js'
+import { expect, test } from 'vitest'
 
+test('render del mazo regular', () => {
+  render(<RegularDeck />)
 
-test('render del mazo regular', async () => {
-  // ARRANGE
-    render(<RegularDeck onClick={() => {}} />)
-  
-  // ASSERT
   const imgs = screen.getAllByRole('img')
-
-
-  expect(imgs.length).toBeGreaterThan(0)
+  expect(imgs.length).toBeGreaterThan(0) // el mazo no está vacío
 })
 
-
-test('primera carta de la pila es murder escapes', async () => {
-  render(<RegularDeck onClick={()=> {}}/>)
+test('la primera carta del mazo regular es Murder Escapes', () => {
+  render(<RegularDeck />)
 
   const imgs = screen.getAllByRole('img')
-  expect(imgs[0]).toHaveAttribute('alt', 'MurderEscapes')
+  const firstCard = imgs[0] // index === 0 -> la de abajo de todo
+  expect(firstCard).toHaveAttribute('src', '/cards/02-murder_escapes.png')
+  expect(firstCard).toHaveAttribute('alt', 'MurderEscapes')
+})
 
+test('todas las demás cartas tienen el reverso', () => {
+  render(<RegularDeck />)
+
+  const imgs = screen.getAllByRole('img')
+  const rest = imgs.slice(1) // todas menos la primera
+  rest.forEach(img => {
+    expect(img).toHaveAttribute('src', '/cards/01-card_back.png')
+  })
 })
