bc9626f1355067c7d57fac5268c1924706dfa5b2
candelalu <candela.saracho@mi.unc.edu.ar>
2025-10-13 16:15:56 -0300

    ING-17 mostrar boton volver a home  al finalizar la partida

--- lab-inge/src/containers/game/components/EndGameDialog/EndGameDialog.jsx
+++ lab-inge/src/containers/game/components/EndGameDialog/EndGameDialog.jsx
@@ -1,25 +1,31 @@
-import { useState, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom"
+import "./EndGameDialog.css"
 
-import './EndGameDialog.css'
+export default function EndGameDialog({ winners }) {
+  const navigate = useNavigate()
 
-export default function EndGameDialog({ onClose, winners }) {
-  
+  const handleReturnHome = () => {
+    navigate("/home") 
+  }
 
   return (
     <div className="dialog-backdrop">
       <div className="dialog">
         <h2>Game Over</h2>
+
         <div className="winners-section">
           <h3>Winners:</h3>
           <ul className="winners-list">
             {winners.map((winner) => (
               <li key={winner.id} className="winner-item">
-                {winner.name} 
+                {winner.name}
               </li>
             ))}
           </ul>
         </div>
+        <button className="dialog-button" onClick={handleReturnHome}>
+          Back to Home
+        </button>
       </div>
     </div>
   )
