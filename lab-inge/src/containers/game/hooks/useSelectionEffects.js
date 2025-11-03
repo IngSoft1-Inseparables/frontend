d5d2ce71b541c91d916a37a55035993fac6b9195
toni <antonio.garcia@mi.unc.edu.ar>
2025-11-03 19:49:31 -0300

    SCRUM-182 vuelvo a estado anterior para solucionar bug ocasionado en commit 2023366

--- lab-inge/src/containers/game/hooks/useSelectionEffects.js
+++ lab-inge/src/containers/game/hooks/useSelectionEffects.js
@@ -165,13 +165,15 @@ export const useSelectionEffects = (
 
           await fetchGameData();
 
+          setSelectedPlayer(null);
+          setSelectionAction(null);
+          setFromPlayer(null);
+
         } catch (error) {
           console.error("Error al asignar secreto:", error);
-        } finally {
-          setSelectedPlayer(null);
-          setSelectedSecret(null);
           setFromPlayer(null);
           setSelectionAction(null);
+          setSelectedSecret(null);
         }
       })();
 
@@ -193,7 +195,7 @@ export const useSelectionEffects = (
         selectedPlayer
       );
       handleStealSet(selectedPlayer, selectedSet);
-      
+
     }
   }, [selectionMode, selectedSet, selectedPlayer]);
 
